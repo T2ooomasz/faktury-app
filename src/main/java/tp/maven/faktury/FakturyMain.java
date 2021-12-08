3f148be388b03e4577ead4b4c2a88acbc44fb71e
T2ooomasz <tomkazulak@gmail.com>
2021-12-08 01:04:16 +0100

    Initial commit

---
+++ src/main/java/tp/maven/faktury/FakturyMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tp.maven.faktury;
+
+/**
+ * @author tkazulak
+ *
+ */
+public class FakturyMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
